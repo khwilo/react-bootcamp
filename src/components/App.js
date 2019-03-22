41873790a51968d0a6204698b537df6a82ab7cb2
khwilo <khwilowatai@gmail.com>
2019-03-22 12:46:09 +0300

    Use react router to render the different components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import About from './About';
 import Home from './Home';
@@ -7,12 +8,21 @@ import Topics from './Topics';
 class App extends Component {
   render() {
     return (
-      <div>
-        React Bootcamp
-        <Home />
-        <About />
-        <Topics />
-      </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/topics" component={Topics} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
