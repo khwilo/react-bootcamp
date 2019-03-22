56e34e6ad8b0055c06517eb94c5c4d8aeb5a31ae
khwilo <khwilowatai@gmail.com>
2019-03-22 12:09:06 +0300

    Create home, about and topics module

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import About from './About';
+import Home from './Home';
+import Topics from './Topics';
+
 class App extends Component {
   render() {
     return (
       <div>
         React Bootcamp
+        <Home />
+        <About />
+        <Topics />
       </div>
     );
   }
---
+++ src/components/Topics.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Topic = () => {
+    return <div>TOPICS</div>;
+};
+
+export default Topic;
