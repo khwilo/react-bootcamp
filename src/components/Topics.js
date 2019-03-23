6e0f9c163ba03ad805f73edf727be10a79073e59
khwilo <khwilowatai@gmail.com>
2019-03-23 10:24:53 +0300

    Implement nested routes

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 const Topic = () => {
-    return <div>TOPICS</div>;
+    return <div>Topic</div>;
 };
 
-export default Topic;
+const Topics = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <h2>Topics</h2>
+                <ul>
+                    <li>
+                        <Link to="/topics/rendering">Rendering with React</Link>
+                    </li>
+                    <li>
+                        <Link to="/topics/components">Components</Link>
+                    </li>
+                    <li>
+                        <Link to="/topics/props-v-state">Props Vs state</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Route path="/topics/rendering" component={Topic} />
+                <Route path="/topics/components" component={Topic} />
+                <Route path="/topics/props-v-state" component={Topic} />
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default Topics;
